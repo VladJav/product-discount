273d584c946692a1755a153a2549c56f838cbef7
hrom1 <hrom012@gmail.com>
2023-09-15 18:44:28 +0200

    add: basic jest configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: `${__dirname}/../.env` });
 
 const app = express();
-const PORT: number = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response)=>{
-    res.send('Hello World');
+    res.send('Hesslls Wosrld');
 });
 
 app.listen(PORT, ()=>{
